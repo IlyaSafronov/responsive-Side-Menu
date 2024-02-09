65c6371d986063205b59416059c871f66916f954
Ilya Safronov <IlyaSafronov18@gmail.com>
2024-02-10 00:39:52 +0300

    add toggle js

--- script.js
+++ script.js
@@ -2,9 +2,21 @@
 const header = document.getElementById('header');
 const nav_toggler = document.getElementById('nav-toggler');
 const nav = document.getElementById('nav');
+const nav__links = document.querySelectorAll('.nav__item-link');
 
 nav_toggler.addEventListener('click', function() {
   this.classList.toggle('fa-times');
   nav.classList.toggle('collapse');
+  header.classList.toggle('collapse-header');
+});
 
-})
+nav__links.forEach((link) => {
+  link.addEventListener('click', function() {
+    nav__links.forEach((l) => {
+      if (l.classList.contains('active')) {
+        l.classList.remove('active');
+      }
+    })
+    this.classList.toggle('active');
+  });
+});
