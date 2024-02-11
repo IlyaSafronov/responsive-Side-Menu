b34d380cc0318d58f817ca5fada2c649f7cdf807
Ilya Safronov <IlyaSafronov18@gmail.com>
2024-02-12 01:13:32 +0300

    add toggle link js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const header = document.getElementById('header');
 const nav_toggler = document.getElementById('nav-toggler');
 const nav = document.getElementById('nav');
 const nav__links = document.querySelectorAll('.nav__item-link');
+const sub_links = document.querySelectorAll('.sub_link');
 
 nav_toggler.addEventListener('click', function() {
   this.classList.toggle('fa-times');
@@ -18,5 +19,16 @@ nav__links.forEach((link) => {
       }
     })
     this.classList.toggle('active');
+    const sub_menu = this.nextElementSibling;
+    if (sub_menu) {
+      sub_menu.classList.toggle('d-none');
+    }
+  });
+});
+
+sub_links.forEach((link) => {
+  link.addEventListener('click', () => {
+    sub_links.forEach((l) => l.classList.remove('active-sub-link'));
+    link.classList.toggle('active-sub-link');
   });
 });
